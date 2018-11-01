086b26ab9070810cd32f9232dd31cc34615938cf
Bookenz <boookenz@gmail.com>
2018-11-01 07:49:45 +0100

    Initial Commit

---
+++ src/main/java/com/xcalliber/assignement/datasetcomparator/DatasetComparatorApplication.java
@@ -0,0 +1,12 @@
+package com.xcalliber.assignement.datasetcomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetComparatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xcalliber/assignement/datasetcomparator/DatasetComparatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xcalliber.assignement.datasetcomparator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatasetComparatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
